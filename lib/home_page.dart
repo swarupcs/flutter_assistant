7b2bd7930edb1ddce999cecf73597ad9c7743e1a
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-07-13 14:39:32 +0530

    First UI part done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_assistant/feature_box.dart';
 import 'package:flutter_assistant/pallete.dart';
 
 class HomePage extends StatefulWidget {
@@ -44,10 +45,79 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ],
-            //chat bubble
-
+          ),
+          //chat bubble
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            margin: const EdgeInsets.symmetric(
+                horizontal: 40
+            ).copyWith(top: 30),
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor,
+                ),
+              //top left border corner became not radius
+              borderRadius: BorderRadius.circular(20).copyWith(
+                topLeft: Radius.zero,
+              ),
+            ),
+            child:  const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text('Good Morning, what task can I do for you? ',
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                color: Pallete.mainFontColor,
+                fontSize: 25,
+              ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(
+              top: 10,
+              left: 22,
+            ),
+            child: const Text(
+              'Here are a few features',
+              style: TextStyle(
+                fontFamily: 'Cera Pro',
+                color: Pallete.mainFontColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          // features list
+          const Column(
+            children: [
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: 'ChatGPT',
+                descriptionText: 'A smarter way to stay organized and informe and ChatGPT',
+              ),
+              FeatureBox(
+                color: Pallete.secondSuggestionBoxColor,
+                headerText: 'Dall-E',
+                descriptionText: 'Get inspired and stay creative with your personal assistant powered by Dall-E',
+              ),
+              // FeatureBox(
+              //   color: Pallete.thirdSuggestionBoxColor,
+              //   headerText: 'Smart Voice Assistant',
+              //   descriptionText: 'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+              // ),
+            ],
           )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () {},
+        child: const Icon(Icons.mic),
       ),
     );
   }
